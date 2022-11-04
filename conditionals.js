2b711e59ec824f7eacd2909fbb16c2607e755823
Lian Chan <lian.chan@telus.com>
2022-11-04 01:45:21 -0700

    Added and changed conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,21 @@
-const raining = true;
-const temperature= 17;
+const raining = false;
+const temperature= 25;
 if(raining){
 console.log("Don't forget your umbrella!");
+} 
+if (!raining){
+  console.log("Leave your umbrella at home!");
 }
-if(temperature <0){
-console.log("Make sure you pick out a scarf!");
-} else if(temperature < 15){
-  console.log("Short sleeves won't cut it!");
+if(temperature <-40 || temperature > 40){
+console.log("Maybe going outside isn't such a great idea..");
 } else {
-  console.log("Short sleeves are fine.");
+  console.log("It is okay to go out!");
+} 
+
+
+
+const isCitizen = true;
+const age = 26;
+if(isCitizen && age > 18){
+  console.log("You are eligible to vote");
 }
-console.log("Now you're ready to go outside!");
